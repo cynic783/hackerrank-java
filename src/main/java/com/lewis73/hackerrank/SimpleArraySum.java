787f608ccd175b5770ae5eca79c67aa0f93a3a53
Dan Lewis <daniel.lewis@ext.biomerieux.com>
2018-01-16 14:36:56 -0600

    rename vars

--- src/main/java/com/lewis73/hackerrank/SimpleArraySum.java
+++ src/main/java/com/lewis73/hackerrank/SimpleArraySum.java
@@ -5,28 +5,28 @@ import java.util.stream.IntStream;
 
 public class SimpleArraySum {
 
-    static int simpleArraySum(int n, int[] ar) {
+    static int simpleArraySum(int n, int[] arr) {
         if (n < 0) {
             throw new IllegalArgumentException("n is negative");
         }
-        if (ar.length < n) {
+        if (arr.length < n) {
             throw new IllegalArgumentException("array does not contain " + n + " elements");
         }
-        if (ar.length > n) {
-            throw new IllegalArgumentException("expected array to have " + n + " elements but found " + ar.length + " elements");
+        if (arr.length > n) {
+            throw new IllegalArgumentException("expected array to have " + n + " elements but found " + arr.length + " elements");
         }
 
-        return IntStream.of(ar).sum();
+        return IntStream.of(arr).sum();
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[] ar = new int[n];
-        for(int ar_i = 0; ar_i < n; ar_i++){
-            ar[ar_i] = in.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = in.nextInt();
         }
-        int result = simpleArraySum(n, ar);
+        int result = simpleArraySum(n, arr);
         System.out.println(result);
     }
 }
